b443dfb587d7c09329a832628a880d37040a4481
zhangyunze <839423896@qq.com>
2020-03-13 09:42:26 +0800

    我的主页

--- src/admin/index/home.vue
+++ src/admin/index/home.vue
@@ -1,5 +1,14 @@
 <template>
-    <div>我的主页</div>
+    <div>
+        <div>
+            <el-carousel indicator-position="outside">
+            <el-carousel-item v-for="item in 4" :key="item">
+            <h3>{{ item }}</h3>
+            </el-carousel-item>
+        </el-carousel>
+        </div>
+    </div>
+    
 </template>
 <script>
 export default {
